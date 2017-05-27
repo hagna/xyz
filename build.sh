753a2388ebf0722c60d3fca65bb3c169aa0180ff
family <natester@gmail.com>
2017-05-27 17:44:21 -0600

    add darwin

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ GOOS="freebsd" GOARCH="amd64" go build
 cp X ../../bdist/X_freebsd_amd64
 GOOS="linux" GOARCH="arm" GOARM="7"
 cp X ../../bdist/X_linux_arm
+GOOS="darwin" GOARCH="amd64" go build
+cp X ../../bdist/X_darwin_amd64
 
 cd ../Y
 go build
@@ -21,6 +23,8 @@ GOOS="freebsd" GOARCH="amd64" go build
 cp Y ../../bdist/Y_freebsd_amd64
 GOOS="linux" GOARCH="arm" GOARM="7"
 cp Y ../../bdist/Y_linux_arm
+GOOS="darwin" GOARCH="amd64" go build
+cp Y ../../bdist/X_darwin_amd64
 
 cd ../Z
 go build
@@ -31,6 +35,8 @@ GOOS="freebsd" GOARCH="amd64" go build
 cp Z ../../bdist/Z_freebsd_amd64
 GOOS="linux" GOARCH="arm" GOARM="7"
 cp Z ../../bdist/Z_linux_arm
+GOOS="darwin" GOARCH="amd64" go build
+cp Z ../../bdist/X_darwin_amd64
 
 
 
