d9165641a58d070d15a83ac47687548fdca0b3d3
family <natester@gmail.com>
2017-05-27 17:30:17 -0600

    add build script take 1

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+cd cmd/X
+go build
+cp X ../../bdist/X_linux_amd64
+GOOS="windows" GOARCH="amd64" go build
+cp X ../../bdist/X_windows_amd64.exe
+GOOS="freebsd" GOARCH="amd64" go build
+cp X ../../bdist/X_freebsd_amd64
+GOOS="linux" GOARCH="arm" GOARM="7"
+cp X ../../bdist/X_linux_arm
+
+cd ../Y
+go build
+cp Y ../../bdist/Y_linux_amd64
+GOOS="windows" GOARCH="amd64" go build
+cp Y ../../bdist/Y_windows_amd64.exe
+GOOS="freebsd" GOARCH="amd64" go build
+cp Y ../../bdist/Y_freebsd_amd64
+GOOS="linux" GOARCH="arm" GOARM="7"
+cp Y ../../bdist/Y_linux_arm
+
+cd ../Z
+go build
+cp Z ../../bdist/Z_linux_amd64
+GOOS="windows" GOARCH="amd64" go build
+cp Z ../../bdist/Z_windows_amd64.exe
+GOOS="freebsd" GOARCH="amd64" go build
+cp Z ../../bdist/Z_freebsd_amd64
+GOOS="linux" GOARCH="arm" GOARM="7"
+cp Z ../../bdist/Z_linux_arm
+
+
+
+
